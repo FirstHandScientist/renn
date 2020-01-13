e0d63837114f96ddd014f21c079c6b56d0b287f5
Dong Liu <doli@kth.se>
2020-01-13 14:55:19 +0100

    back to include one binary_marginals in approximation

--- gins/utils/utils.py
+++ gins/utils/utils.py
@@ -21,15 +21,15 @@ def get_scores(true_ub, test_ub):
     """
     unary_marginals, binary_marginals = true_ub
     unary_marginals_mf, binary_marginals_mf = test_ub
-    marginals = torch.cat([unary_marginals, binary_marginals.reshape(-1)], 0)
-    marginals_mf = torch.cat([unary_marginals_mf, binary_marginals_mf.reshape(-1)], 0)
+    marginals = torch.cat([unary_marginals, binary_marginals[:, 1, 1]], 0)
+    marginals_mf = torch.cat([unary_marginals_mf, binary_marginals_mf[:, 1, 1]], 0)
 
     corr_unary_mf = corr(unary_marginals, unary_marginals_mf)
-    corr_binary_mf = corr(binary_marginals.reshape(-1), binary_marginals_mf.reshape(-1))
+    corr_binary_mf = corr(binary_marginals[:, 1, 1], binary_marginals_mf[:, 1, 1])
     corr_mf = corr(marginals, marginals_mf)
 
     l1_unary_mf = l1(unary_marginals, unary_marginals_mf)
-    l1_binary_mf = l1(binary_marginals.reshape(-1), binary_marginals_mf.reshape(-1))
+    l1_binary_mf = l1(binary_marginals[:, 1, 1], binary_marginals_mf[:, 1, 1])
     l1_mf = l1(marginals, marginals_mf)
 
     return (l1_mf, corr_mf)
