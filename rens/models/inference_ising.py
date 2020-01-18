train by renn works (fixed by enable gradient of log_phi factors)

rens/models/inference_ising.py
@@ -207,10 +207,25 @@ class kikuchi_net_infer(torch.nn.Module):
             return (-kikuchi_energy, -consist_error, match_node_num)
 
     def neg_free_energy(self):
-        kikuchi_energy, consist_error = self.encoder(self.model.region_graph)
+        """Compute the Kikuchi free energy"""
+        # kikuchi_energy, consist_error = self.encoder(self.model.region_graph)
+        graph = self.model.region_graph
+        energy = 0
+        belief_name = 'net_belief'
+        self.model._init_disfactor()
+        self.model.attach_region_factors(graph)
+        for node in graph.nodes():
+            graph.nodes[node][belief_name].values = torch.clamp(graph.nodes[node][belief_name].values.detach(), min=1e-8)
+            energy += torch.sum(graph.nodes[node][belief_name].values * \
+                       (graph.nodes[node][belief_name].values.log() - \
+                        graph.nodes[node]['log_phi'].values)) * \
+                        graph.nodes[node]['weight']
+
+        # energy
+        
         match_node_num = int(self.model.region_graph.number_of_nodes()) - \
             len(self.model.region_graph.region_layers['R0'])
-        return (-kikuchi_energy, -consist_error, match_node_num)
+        return (-energy, 0, match_node_num)
 
 
         

rens/models/ising.py
@@ -156,6 +156,7 @@ class GeneralizedInferenceNetwork(TransformerInferenceNetwork):
         """Compute the Kikuchi free energy"""
         energy = 0
         for node in graph.nodes():
+            graph.nodes[node][self.belief_name].values = torch.clamp(graph.nodes[node][self.belief_name].values, min=1e-8)
             energy += torch.sum(graph.nodes[node][self.belief_name].values * \
                        (graph.nodes[node][self.belief_name].values.log() - \
                         graph.nodes[node]['log_phi'].values.detach())) * \
@@ -972,7 +973,7 @@ class Ising(nn.Module):
             bethe += binary_factor_ij.sum()
         return bethe
 
-    def trainer(self, dataloader, infer_method, num_epoch, optimizer, max_norm, is_net):
+    def trainer(self, dataloader, infer_method, num_epoch, optimizer, max_norm, agg_pen,is_net):
         """
         Do the learning of ising model with given dataset, and inference method.
         """
@@ -985,7 +986,8 @@ class Ising(nn.Module):
                     log_Z, _, _ = infer_method()
                 else:
                     neg_free_energy, consist_error, match_node_num = infer_method()
-                    log_Z = neg_free_energy + consist_error * 1.0 / float(match_node_num)
+                    
+                    log_Z = neg_free_energy + consist_error * agg_pen
                     
                 loss += log_Z
                 loss = loss.sum() / batch.size(0)